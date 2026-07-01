31f8d43ade9ab7a09b5178e169d29383b9224fe7
Ingenium-Team <ingeniummteam@gmail.com>
2026-07-02 01:10:27 +0300

    Add files via upload

---
+++ js/supabase.js
@@ -0,0 +1,10 @@
+const SUPABASE_URL = "https://ectgevydafkosrarnclr.supabase.co";
+const SUPABASE_KEY = "sb_publishable_G-8lMbxKgoMMxAcd5hOsBg_dz4v_Jt-";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
+
+console.log("✅ Supabase Connected");
+console.log(supabaseClient);
